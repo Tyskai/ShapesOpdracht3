f150b7427015bababd3096803f027959cbf3766a
Tyskai <iamtyskai@gmail.com>
2014-01-14 20:58:02 +0100

    Toegevoegt en WERKEND GetCornetPoints
    Nu zijn de methods voor GetcornerPoint werkend en het werkt allemaal
    weer op mijn laptop :)

--- ShapeDrawing/ShapeDrawing/Rectangle.cs
+++ ShapeDrawing/ShapeDrawing/Rectangle.cs
@@ -22,25 +22,55 @@ class Rectangle : Shape
 	public override void Draw(Graphics Canvas)
     {
 		Pen pen = new Pen(Color.Black);
-		Canvas.DrawLine(pen,x,y,x + width,y);
-		Canvas.DrawLine(pen,x+width,y,x+width,y+height);
-		Canvas.DrawLine(pen,x+width,y+height,x,y+height);
-		Canvas.DrawLine(pen,x,y+height,x,y);
+
+        Point[] points = GetCornerPoints();
+
+        Canvas.DrawLine(pen, points[0], points[1]);
+        Canvas.DrawLine(pen, points[1], points[2]);
+        Canvas.DrawLine(pen, points[2], points[3]);
+        Canvas.DrawLine(pen, points[3], points[0]);
     }
 
     /// <summary>
     /// This function gives back the points of the rectangle
     /// </summary>
     /// <returns>A list containing the four corners of a recctangle</returns>
-    public List<Point> GetCornerPoints()
+    public Point[] GetCornerPoints()
     {
-        Point a = new Point();
-        a.X = 4;
-        a.Y = 3;
 
-        List<Point> pointList = new List<Point>();
+        int numerOfPoints = 4;
 
-        pointList.Add(a);
+        //THe list containing the corner points
+        Point[] pointList = new Point[numerOfPoints];
+
+        //upper left point
+        Point ul = new Point();
+        ul.X = x;
+        ul.Y = y;
+
+        //upper right point
+        Point ur = new Point();
+        ur.X = x + width;
+        ur.Y = y;
+
+
+        //lower right point
+        Point lr = new Point();
+        lr.X = x + width;
+        lr.Y = y + height;
+
+
+        //lower left point
+        Point ll = new Point();
+        ll.X = x;
+        ll.Y = y + height;
+
+
+        //Add the points to the list
+        pointList[0] = ul;
+        pointList[1] = ur;
+        pointList[2] = lr;
+        pointList[3] = ll;
 
         return pointList;
     }
