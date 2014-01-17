85dd67944158c8c1cf4ea107198cd3e0d90aa37a
chiel laptop <m.a.vangriethuijsen@students.uu.nl>
2014-01-17 12:11:28 +0100

    regenboog kleuren

--- ShapeDrawing/ShapeDrawing/VisualSVG.cs
+++ ShapeDrawing/ShapeDrawing/VisualSVG.cs
@@ -9,7 +9,7 @@ class VisualSVG : Visual
     //Draw a SVG line
     public override string DrawLine(Point p1, Point p2, Color color)
     {
-        string line = "<polyline points='" + p1.X + "," + p1.Y + "  " + p2.X + "," + p2.Y + " " + "' style='fill:none;stroke:" + color.ToString() + ";stroke-width:1'/>";
+        string line = "<polyline points='" + p1.X + "," + p1.Y + "  " + p2.X + "," + p2.Y + " " + "' style='fill:none;stroke:" + getRGBstring(color) + ";stroke-width:1'/>";
         return line;
     }
 
@@ -18,8 +18,13 @@ class VisualSVG : Visual
     {
         int r = s / 2;
 
-        string ellipse = "<circle cx='" + (x + r) + "' cy='" + (y + r) + "' r='" + r + "' stroke-width='1' fill='none' stroke='" + color.ToString() + "' />";
+        string ellipse = "<circle cx='" + (x + r) + "' cy='" + (y + r) + "' r='" + r + "' stroke-width='1' fill='none' stroke='" + getRGBstring(color)  + "' />";
         return ellipse;
+    }
+
+    public string getRGBstring(Color color)
+    {
+        return "rgb(" + color.R + " ," + color.G + " ," + color.B+")";
     }
 
 }
